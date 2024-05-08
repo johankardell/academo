14a25776308c3e9d3e349bba925abdfc4b7d9fb4
Johan Kardell <johan.kardell@gmail.com>
2024-05-08 09:28:27 +0000

    Update loadtest

--- loadtest/hey.sh
+++ loadtest/hey.sh
@@ -1,1 +1,1 @@
-hey -n 100000 -c 100 -m GET https://externalapi.lemonpond-2f184610.swedencentral.azurecontainerapps.io/id
+hey -n 100000 -c 100 -m GET https://externalapi.happytree-90babb39.swedencentral.azurecontainerapps.io/id
