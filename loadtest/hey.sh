bf97614038e7a89f12636fa1f9134df528d42a79
Johan Kardell <johan.kardell@gmail.com>
2024-10-14 16:09:55 +0200

    bump version to remediate vulnerabilities

--- loadtest/hey.sh
+++ loadtest/hey.sh
@@ -1,1 +1,1 @@
-hey -n 100000 -c 100 -m GET https://externalapi.happytree-90babb39.swedencentral.azurecontainerapps.io/id
+hey -n 100000 -c 100 -m GET https://externalapi.grayfield-8697b488.swedencentral.azurecontainerapps.io/id
